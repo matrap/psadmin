3f6f22914e826b9348f81fd0a2b436e4656e1f89
Mateusz Rapacz <mateusz.rapacz@comarch.pl>
2017-04-19 17:16:04 +0200

    Validation authorForm. Toastr library

--- src/components/authors/manageAuthorPage.js
+++ src/components/authors/manageAuthorPage.js
@@ -2,6 +2,7 @@
 
 var React = require('react');
 var Router = require('react-router');
+var Toastr = require('toastr');
 var AuthorForm = require('./authorForm');
 var AuthorApi = require('../../api/authorApi');
 
@@ -12,9 +13,28 @@ var ManageAuthorPage = React.createClass({
 
     getInitialState: function() {
         return {
-            author: {id: '', firstName: '', lastName: ''}
+            author: {id: '', firstName: '', lastName: ''},
+            errors: {}         
         };
     },
+
+	authorFormIsValid: function() {
+		var formIsValid = true;
+		this.state.errors = {}; //clear any previous errors.
+
+		if (this.state.author.firstName.length < 3) {
+			this.state.errors.firstName = 'First name must be at least 3 characters.';
+			formIsValid = false;
+		}
+
+		if (this.state.author.lastName.length < 3) {
+			this.state.errors.lastName = 'Last name must be at least 3 characters.';
+			formIsValid = false;
+		}
+
+		this.setState({errors: this.state.errors});
+		return formIsValid;
+    },    
 
     setAuthorState: function(event) {
         var field = event.target.name;
@@ -25,7 +45,13 @@ var ManageAuthorPage = React.createClass({
 
     saveAuthor: function(event) {
         event.preventDefault();
+
+        if(!this.authorFormIsValid()) { 
+            return;
+        }
+
         AuthorApi.saveAuthor(this.state.author);
+        Toastr.success('Author saved.');
         this.transitionTo('authors');
     },
 
@@ -35,7 +61,8 @@ var ManageAuthorPage = React.createClass({
                 <AuthorForm 
                     author={this.state.author}
                     onChange={this.setAuthorState}
-                    onSave={this.saveAuthor}/>
+                    onSave={this.saveAuthor}
+                    errors={this.state.errors}/>
             </div>
         );
     }
